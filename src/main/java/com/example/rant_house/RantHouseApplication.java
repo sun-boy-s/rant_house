324da4435bc2a6e7582bd93107c6312bf9abad6a
lyk123456 <7512421+lyk123456@user.noreply.gitee,com>
2020-09-11 13:54:40 +0800

    短租网_第一次提交

---
+++ src/main/java/com/example/rant_house/RantHouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.rant_house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RantHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RantHouseApplication.class, args);
+    }
+
+}
